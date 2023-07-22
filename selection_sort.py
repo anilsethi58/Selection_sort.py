4cca6113de7649bf3064f4916b34d79e7e617cfb
ANIL SETHI <136245167+anilsethi58@users.noreply.github.com>
2023-07-22 19:49:01 +0530

    Add files via upload

---
+++ selection_sort.py
@@ -0,0 +1,20 @@
+def Selection_Sort(array):
+    for i in range(0, len(array) - 1):
+        smallest = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[smallest]:
+                smallest = j
+        array[i], array[smallest] = array[smallest], array[i]
+
+array = input('Enter the list of numbers: ').split()
+array = [int(x) for x in array]
+Selection_Sort(array)
+print('List after sorting is : ', end='')
+print(array)
+
+
+
+
+#output
+# Enter the list of numbers: 3 6 8 9 65 2 
+# List after sorting is : [2, 3, 6, 8, 9, 65]
